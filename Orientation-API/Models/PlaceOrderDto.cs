470efdf84808c5b032d50914e950badce67d8d6f
Kelly Stoops <kstoops815@gmail.com>
2018-04-09 21:08:20 -0500

    place order dto created

---
+++ Orientation-API/Models/PlaceOrderDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Orientation_API.Models
+{
+    public class PlaceOrderDto
+    {
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public int SalesRepId { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
