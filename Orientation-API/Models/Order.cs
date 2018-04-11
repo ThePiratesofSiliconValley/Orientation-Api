1d30f4573c69ed05f9f7748059168444a0ca39ec
Ben Harrington <harringtonben@icloud.com>
2018-04-11 17:42:39 -0500

    finished getting all orders with outstanding payment, but going back and changing the return for security

---
+++ Orientation-API/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Orientation_API.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public int TotalProducts { get; set; }
+        public double TotalPrice { get; set; }
+        public int PaymentTypeId { get; set; }
+        public int SalesRepId { get; set; }
+    }
+}
