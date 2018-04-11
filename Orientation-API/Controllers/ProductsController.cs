d67df2601d3cd83a6130109b286412c69ed6e342
David Dassau <todieahero@hotmail.com>
2018-04-10 20:20:16 -0500

    completed remove product methods!

--- Orientation-API/Controllers/ProductsController.cs
+++ Orientation-API/Controllers/ProductsController.cs
@@ -40,26 +40,18 @@ namespace Orientation_API.Controllers
         {
             var productRepository = new ProductRepository();
             var singleProduct = productRepository.GetSingleProduct(productId);
-            var removeProduct = productRepository.Remove(productId);
-
-            //check if product, if not 404
+            
             if (singleProduct == null)
             {
                 return Request.CreateErrorResponse(HttpStatusCode.NotFound, "Could not find product");
             }
-            else if (removeProduct)
+            else if (productRepository.Remove(productId))
             {
                 return Request.CreateResponse(HttpStatusCode.OK);
             }
             else
                 return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Sorry about your luck");
-
-
-            //remove product
-
-            //check if success, if not 500, if yes 200
             
-
         }
     }
 }
