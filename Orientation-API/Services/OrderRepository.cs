128300e658f4606f8fba0af1f46b5b9504a49097
Kelly Stoops <kstoops815@gmail.com>
2018-04-11 17:50:33 -0500

    create order repo and dto created

--- Orientation-API/Services/OrderRepository.cs
+++ Orientation-API/Services/OrderRepository.cs
@@ -11,7 +11,17 @@ namespace Orientation_API.Services
 {
     public class OrderRepository
     {
-        //public bool CreateOrder();
+        public bool CreateOrder(CreateOrderDto createOrderDto)
+        {
+            using (var db = new SqlConnection(ConfigurationManager.ConnectionStrings["Main"].ConnectionString))
+            {
+                db.Open();
+
+                var orderCreated = db.Execute("@INSERT into Orders (CustomerId, SalesRepId) VALUES (@CustomerId, @SalesRepId)", createOrderDto);
+
+                return orderCreated == 1;
+            }
+        }
 
         public bool AddProductToOrder(PlaceOrderDto placeOrderDto)
         {
@@ -25,5 +35,11 @@ namespace Orientation_API.Services
                 return orderPlaced == 1;
             }
         }
+    }
+
+    public class CreateOrderDto
+    {
+        public int CustomerId { get; set; }
+        public int SalesRepId { get; set; }
     }
 }
