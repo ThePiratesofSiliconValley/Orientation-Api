d1f13d67982fe9232fc282bb4aae24fe35d470ed
Ben Harrington <harringtonben@icloud.com>
2018-04-23 20:45:30 -0500

    get all method is written, going to test now

--- Orientation-API/Controllers/ComputersController.cs
+++ Orientation-API/Controllers/ComputersController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Orientation_API.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -13,7 +14,10 @@ namespace Orientation_API.Controllers
         [HttpGet, Route("")]
         public HttpResponseMessage Get()
         {
-            return Request.CreateResponse(HttpStatusCode.OK);
+            var repo = new ComputerRespository();
+            var results = repo.GetAllComputers();
+
+            return Request.CreateResponse(HttpStatusCode.OK, results);
         }
     }
 }
