ee1810cdb7a7a52de5eda123778076295f45f5e3
Caitlin Murnick <cmurnick@benefitcommunications.com>
2018-04-23 21:29:16 -0500

    working on API

---
+++ Orientation-API/Controllers/TrainingProgramsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Orientation_API.Models;
+using Orientation_API.Services;
+
+namespace Orientation_API.Controllers
+{
+    [RoutePrefix("api/trainings")]
+    public class TrainingProgramsController
+    {
+        [HttpPost, Route("")]
+        public HttpResponseMessage CreateTraining(TrainingProgramDto training)
+        {
+            var repository = new TrainingProgramRepository();
+            var result = repository.CreateTraining(training);
+
+            if (result)
+            {
+                return Request.CreateResponse(HttpStatusCode.Created, "Training has been created");
+            }
+
+            return Request.CreateResponse(HttpStatusCode.InternalServerError, "Training didn't save, try again");
+        }
+        
+        
+    }
+}
