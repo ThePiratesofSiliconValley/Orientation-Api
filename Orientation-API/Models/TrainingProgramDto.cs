fb27e06128b36d6aee47c52e579d9700ac88630f
Caitlin Murnick <cmurnick@benefitcommunications.com>
2018-04-23 20:49:52 -0500

    fixing

--- Orientation-API/Models/TrainingProgramDto.cs
+++ Orientation-API/Models/TrainingProgramDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,9 @@ namespace Orientation_API.Models
 {
     public class TrainingProgramDto
     {
+        public string TrainingName { get; set; }
+        public DateTime StartDayDate { get; set; }
+        public DateTime EndDayDate { get; set; }
+        public int MaxAttendees { get; set; }
     }
 }
