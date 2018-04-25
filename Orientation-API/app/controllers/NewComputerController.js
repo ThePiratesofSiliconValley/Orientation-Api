0ab1c52723fee8a5a9f7be3924772ebb4ea98a91
Ben Harrington <harringtonben@icloud.com>
2018-04-25 18:25:02 -0500

    data is posting correctly, now I am going to style

--- Orientation-API/app/controllers/NewComputerController.js
+++ Orientation-API/app/controllers/NewComputerController.js
@@ -1,1 +1,25 @@
-﻿
+﻿app.controller("NewComputerController", ["$scope", "$http", "$location", function ($scope, $http, $location) {
+
+    $scope.addComputer = {};
+
+    var createNewComputer = function (computer) {
+        return $http.post("api/computers", JSON.stringify(computer));
+    };
+
+    $scope.submitForm = function () {
+ 
+        var newComputer = {
+            "computermanufacturer": $scope.addcomputer.computermanufacturer,
+            "computermake": $scope.addcomputer.computermake,
+            "purchasedate": $scope.addcomputer.purchasedate
+        };
+
+        createNewComputer(newComputer).then(function () {
+            $location.path("/computers");
+        }).catch(function (error) {
+            console.log(error);
+        });
+
+    };
+
+}]);
