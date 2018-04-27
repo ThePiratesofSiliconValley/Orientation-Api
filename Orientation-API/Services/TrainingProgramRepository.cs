2eb0a06783a4dcc0c8d44b264121e1b727743264
Caitlin Murnick <cmurnick@benefitcommunications.com>
2018-04-26 22:18:08 -0500

    works on my machine

---
+++ Orientation-API/Services/TrainingProgramRepository.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Dapper;
+using Orientation_API.Controllers;
+using Orientation_API.Models;
+
+namespace Orientation_API.Services
+{
+    public class TrainingProgramRepository
+    {
+        public bool CreateTraining(TrainingProgramDto training)
+        {
+            using (var db = new SqlConnection(ConfigurationManager.ConnectionStrings["Main"].ConnectionString))
+            {
+                db.Open();
+
+                var createTraining = db.Execute(@"INSERT INTO [dbo].[TrainingPrograms]
+                                           ([TrainingName]
+                                           ,[StartDay]
+                                           ,[EndDay]
+                                           ,[MaxAttendees])
+                                     VALUES
+                                           (@TrainingName
+                                           ,@StartDay
+                                           ,@EndDay
+                                           ,@MaxAttendees)", training);
+                return createTraining == 1;
+            }
+        }
+
+        public IEnumerable<TrainingProgramDto> GetAllTrainings()
+        {
+            using (var db = new SqlConnection(ConfigurationManager.ConnectionStrings["Main"].ConnectionString))
+            {
+                db.Open();
+                var trainings = db.Query<TrainingProgramDto>("select * from TrainingPrograms");
+                return trainings;
+            }
+        }
+    }
+}
