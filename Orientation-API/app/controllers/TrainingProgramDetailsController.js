6e694b5e2e338d73450395e3b6ecf809f71f9966
David Dassau <todieahero@hotmail.com>
2018-04-30 12:06:42 -0500

    added TrainingProgramDetailsController.js to controllers.

---
+++ Orientation-API/app/controllers/TrainingProgramDetailsController.js
@@ -0,0 +1,15 @@
+﻿app.controller("TrainingProgramDetailsController", ["$scope", "$http", "$location",
+    function ($scope, $http, $location) {
+
+        $scope.message = "Training Program Details Page";
+
+        //$http.get("/api/trainings").then(function (result) {
+        //    $scope.trainings = result.data;
+
+        //});
+
+        //$scope.createTraining = (trainingId) => {
+        //    $location.path("trainings/new");
+        //};
+    }
+]);
