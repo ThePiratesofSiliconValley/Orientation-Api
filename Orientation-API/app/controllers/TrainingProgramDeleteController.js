587b1dc44e68cf6851da5456d79459f014afe56b
David Dassau <todieahero@hotmail.com>
2018-05-02 11:31:15 -0500

    delete button functionality is working

--- Orientation-API/app/controllers/TrainingProgramDeleteController.js
+++ Orientation-API/app/controllers/TrainingProgramDeleteController.js
@@ -3,5 +3,25 @@
 
         $scope.message = "Training Program Delete Page";
 
+        $scope.training = {};
+
+        function getTraining() {
+            $http.get(`api/trainings/${$routeParams.id}`).then(function (result) {
+                $scope.training = result.data;
+            }).catch(function (error) {
+                console.log("error in training program details", error);
+            });
+        }
+
+        getTraining();
+
+        $scope.deleteTrainingProgram = function () {
+            $http.delete(`api/trainings/${$routeParams.id}`).then(function () {
+                $location.path("/trainings");
+            }).catch(function (error) {
+                console.log("error in deleteTrainingProgram", error);
+            });
+        };
+
     }
 ]);
